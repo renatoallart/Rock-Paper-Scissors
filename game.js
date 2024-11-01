fccd8e143cc1cf1053097dfd0c8cd59fd1520ced
unknown <renato.senstec@gmail.com>
2024-11-01 12:40:58 -0300

    add: playRound() body: adding a function to check the human and, CPU choices and find who's the winner of the round

--- game.js
+++ game.js
@@ -9,3 +9,58 @@ function getHumanChoice() {
   const humanChoice = prompt("insert ur Choice")
   return humanChoice.toLowerCase()
 }
+
+function playRound(humanChoice, computerChoice) {
+  if (humanChoice === computerChoice) return (draw += 1)
+  if (humanChoice === "rock" && computerChoice === "paper") {
+    computerScore += 1
+    return "U Lose"
+  }
+  if (humanChoice === "rock" && computerChoice === "scissors") {
+    humanScore += 1
+    return "U win"
+  }
+  if (humanChoice === "paper" && computerChoice === "scissors") {
+    computerScore += 1
+    return "U Lose"
+  }
+  if (humanChoice === "paper" && computerChoice === "rock") {
+    humanScore += 1
+    return "U win"
+  }
+  if (humanChoice === "scissors" && computerChoice === "rock") {
+    computerScore += 1
+    return "U Lose"
+  }
+  if (humanChoice === "scissors" && computerChoice === "paper") {
+    humanScore += 1
+    return "U win"
+  }
+}
+function playRound(humanChoice, computerChoice) {
+  if (humanChoice === computerChoice) return (draw += 1)
+  if (humanChoice === "rock" && computerChoice === "paper") {
+    computerScore += 1
+    return "U Lose"
+  }
+  if (humanChoice === "rock" && computerChoice === "scissors") {
+    humanScore += 1
+    return "U win"
+  }
+  if (humanChoice === "paper" && computerChoice === "scissors") {
+    computerScore += 1
+    return "U Lose"
+  }
+  if (humanChoice === "paper" && computerChoice === "rock") {
+    humanScore += 1
+    return "U win"
+  }
+  if (humanChoice === "scissors" && computerChoice === "rock") {
+    computerScore += 1
+    return "U Lose"
+  }
+  if (humanChoice === "scissors" && computerChoice === "paper") {
+    humanScore += 1
+    return "U win"
+  }
+}
